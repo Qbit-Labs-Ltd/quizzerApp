c4fdc28f90d956838021d2b966c9fa68c1c05feb
DooMi- <hurmerintaeemeli@gmail.com>
2025-04-01 14:44:59 +0300

    initial commit

---
+++ src/main/java/com/example/quizzerApp/QuizzerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizzerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quizzerApp/QuizzerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizzerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
