b4b1116855f816cb29925dc06906204d45dec085
Gigaheku <hiiqi@hotmail.com>
2025-04-17 14:33:10 +0300

    Added QuestionRepository with basic query methods

---
+++ src/main/java/com/example/quizzerApp/repository/QuestionRepository.java
@@ -0,0 +1,11 @@
+package com.example.quizzerApp.repository;
+
+import com.example.quizzerApp.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    List<Question> findByQuizId(Long quizId);
+}
