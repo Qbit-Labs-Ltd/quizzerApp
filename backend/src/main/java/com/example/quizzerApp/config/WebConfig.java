a8b0fe31bfc0adf179da83df68a6194053ce36f2
Santeri Kuusisto <kuusisto.santeri@gmail.com>
2025-04-24 15:42:34 +0300

    DeploymentFIx

--- backend/src/main/java/com/example/quizzerApp/config/WebConfig.java
+++ backend/src/main/java/com/example/quizzerApp/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173") // Vite's default port
+                .allowedOrigins("http://localhost:5173","https://quizzerapp-1knb.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*") // Add this to expose headers
